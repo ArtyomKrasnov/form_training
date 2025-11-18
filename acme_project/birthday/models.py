eed82d8eb3760ffc77f7a3745b366bb2d4745a13
Artyom Krasnov <wabiiske@yandex.ru>
2025-11-18 15:03:42 +0300

    fix

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-
+from django.urls import reverse
 from .validators import real_age
 
 
@@ -20,3 +20,7 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+    def get_absolute_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
+    
